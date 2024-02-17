132701ad24f83c4c5d1e9cb9bbce805d5960d76c
nt2311-vn <nguyentoan231196@gmail.com>
2024-02-17 22:16:10 +0700

    Add dotenv package to go and check the api key works

--- main.go
+++ main.go
@@ -1,4 +1,19 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	youtubeAPIKey := os.Getenv("YOUTUBE_API_KEY")
+	fmt.Println(youtubeAPIKey)
 }
