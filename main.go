4906db966f5ee1db8c0d4ab8495662bbe8a1030e
nt2311-vn <nguyentoan231196@gmail.com>
2024-02-17 22:37:28 +0700

    First run test and get error on api key

--- main.go
+++ main.go
@@ -1,19 +1,44 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"os"
 
 	"github.com/joho/godotenv"
+	"google.golang.org/api/option"
+	"google.golang.org/api/youtube/v3"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal(err)
+	errLoadEnv := godotenv.Load()
+	if errLoadEnv != nil {
+		log.Fatal(errLoadEnv)
 	}
 
+	fmt.Println("Load environment varialbe comple")
 	youtubeAPIKey := os.Getenv("YOUTUBE_API_KEY")
-	fmt.Println(youtubeAPIKey)
+
+	ctx := context.Background()
+
+	client, errConnection := youtube.NewService(ctx, option.WithAPIKey(youtubeAPIKey))
+
+	if errConnection != nil {
+		log.Fatalf("Error establishing youtube client %v", errConnection)
+	}
+
+	call := client.Subscriptions.List([]string{"snippet"}).Mine(true)
+
+	subscribeList, errGetList := call.Do()
+
+	if errGetList != nil {
+		log.Fatalf("Error get subscription list %v", errGetList)
+	}
+
+	fmt.Println("List of subscription channel")
+
+	for _, item := range subscribeList.Items {
+		fmt.Printf("ID: %s, Title: %s\n", item.Snippet.ResourceId.ChannelId, item.Snippet.Title)
+	}
 }
