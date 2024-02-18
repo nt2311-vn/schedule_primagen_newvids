69b5ba1d530b2f0d94a3a8c1b250895c6cc3712d
nt2311-vn <nguyentoan231196@gmail.com>
2024-02-18 15:32:48 +0700

    Rewrite main

--- main.go
+++ main.go
@@ -1,4 +1,15 @@
 package main
 
+import (
+	"log"
+
+	"github.com/nt2311-vn/schedule_primagen_newvids/auth"
+)
+
 func main() {
+	establishClient, errClient := auth.GetYoutubeService()
+
+	if errClient != nil {
+		log.Fatalf("Cannot connect to youtube service", errClient)
+	}
 }
